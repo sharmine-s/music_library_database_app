4452812f572098424dc3010a82fe6b4e65c396f5
sharmine-s <sharmine.ss@gmail.com>
2023-05-03 17:50:04 +0100

    completed challenge of refactoring get /albums

--- app.rb
+++ app.rb
@@ -22,13 +22,9 @@ class Application < Sinatra::Base
 
   get "/albums" do
     repo = AlbumRepository.new
-    albums = repo.all
+    @albums = repo.all
 
-    response = albums.map do |album|
-      album.title
-    end.join(", ")
-
-    return response
+    return erb(:album)
   end
 
   get "/albums/:id" do
--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -14,10 +14,9 @@ describe Application do
     it "should return the list of albums" do
       response = get("/albums")
 
-      expected_response = "Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring"
-
       expect(response.status).to eq(200)
-      expect(response.body).to eq(expected_response)
+      expect(response.body).to include("Title: Surfer Rosa")
+      expect(response.body).to include("Released: 1988")
     end
   end
 
