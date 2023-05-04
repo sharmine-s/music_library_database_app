b130e11447cf3cbcbc120d16775ae0c0bff6f50c
Sharmine <sharmine.ss@gmail.com>
2023-05-04 12:03:18 +0100

    added a few more tests for getting the form for /albums/new

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -83,6 +83,9 @@ describe Application do
 
       expect(response.status).to eq(200)
       expect(response.body).to include('<form method="POST" action="/albums">')
+      expect(response.body).to include('<input type="text" name="title" />')
+      expect(response.body).to include('<input type="text" name="release_year" />')
+      expect(response.body).to include('<input type="text" name="artist_id" />')
     end
   end
 
